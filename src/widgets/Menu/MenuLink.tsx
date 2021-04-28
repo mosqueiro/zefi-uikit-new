0d63afdc64fe94fdadd1d670f58da6fe5948e098
ZCore <info@zcore.ch>
2021-04-28 04:01:41 -0300

    Change Href

--- src/widgets/Menu/MenuLink.tsx
+++ src/widgets/Menu/MenuLink.tsx
@@ -4,10 +4,20 @@ import { NavLink } from "react-router-dom";
 const MenuLink: React.FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ href, ...otherProps }) => {
   const isHttpLink = href?.startsWith("http");
 
+  const isZCore =
+    href?.startsWith("https://app.zcore.network") ||
+    href?.startsWith("https://zefi.zcore.network") ||
+    href?.startsWith("/") ||
+    href?.startsWith("https://swap.zcore.network");  
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const Tag: any = isHttpLink ? "a" : NavLink;
   const props = isHttpLink ? { href } : { to: href };
-  return <Tag {...props} {...otherProps} />;
+  if (isZCore) {
+    return <Tag {...props} {...otherProps} />;
+  } else {
+    return <Tag target="_blank" {...props} {...otherProps} />;
+  }
 };
 
 export default MenuLink;
